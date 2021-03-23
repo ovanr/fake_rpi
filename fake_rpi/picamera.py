1d1457d6199b5a51bea39892985d6925af578ea4
Orpheas van Rooij <orpheas.vanrooij@outlook.com>
2021-03-23 11:05:46 +0200

    added more picamera attributes added picamera start_preview method

--- fake_rpi/picamera.py
+++ fake_rpi/picamera.py
@@ -7,7 +7,7 @@ import numpy as np  # type: ignore
 # import platform
 from .wrappers import printf
 from .Base import Base
-
+from typing import Optional, Tuple
 
 class BGR(object):
     """Fake class"""
@@ -33,10 +33,43 @@ class PiCamera(Base):
         # empty constructor
         # print('WARNING: Fake_RPi PiCamera on {}'.format(platform.system().lower()))
         Base.__init__(self, self.__class__)
-        pass
+        
+        # default values as in picamera lib
+        self.sharpness: int = 0
+        self.contrast: int = 0
+        self.brightness: int = 50
+        self.saturation: int = 0
+        self.iso: int = 0 # auto
+        self.video_stabilization: bool = False
+        self.exposure_compensation: int = 0
+        self.exposure_mode: str = 'auto'
+        self.meter_mode: str = 'average'
+        self.awb_mode: str = 'auto'
+        self.image_effect: str = 'none'
+        
+        # color_effects is an optional tuple
+        # of two integers with a range 0-255 inclusive
+        self.color_effects: Optional[Tuple[int,int]] = None
+        self.rotation: int = 0
+        self.hflip: bool = False
+        self.vflip: bool = False
+        self.zoom: Tuple[float, float, float, float] = (0.0, 0.0, 1.0, 1.0)
 
     def __enter__(self):
         return self
+    
+    def start_preview(self, 
+                      resolution=None, 
+                      layer=2, 
+                      alpha=255,
+                      fullscreen=True, 
+                      window=None, 
+                      crop=None, 
+                      rotation=0, 
+                      vflip=False,
+                      hflip=False, 
+                      anamorphic=False):
+        pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
